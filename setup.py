52b096fc40bda1ba06156c517b9a2d6a6b51c2a4
Jash Dubal <73313777+jashdubal@users.noreply.github.com>
2023-10-22 13:13:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="essence-nlp",
-    version="1.0.6",
+    version="1.0.7",
     rust_extensions=[RustExtension("essence_nlp.essence", "src/essence_nlp/essence/Cargo.toml", binding=Binding.PyO3)],
     packages=["essence_nlp"],
     package_dir={"essence_nlp": "src/essence_nlp"},
